100a3dbc029e631ea47efb4fdc1e8dca8a4cbaa8
Neeraj Singnath <Neerajs604086@gmail.com>
2026-07-14 03:03:37 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,27 @@
+from typing import List
+from setuptools import find_packages, setup
+
+
+def get_requirements(path: str) -> List[str]:
+    """
+    function for reading requirements.txt`
+    """
+    requirements = []
+    with open(path) as f:
+        requirements = f.readlines()
+    requirements = [req.replace("\n", "") for req in requirements]
+
+    if "-e ." in requirements:
+        requirements.remove("-e .")
+
+    return requirements
+
+
+setup(
+    name="mlproject",
+    version="0.0.1",
+    author="Neeraj Singnath",
+    author_email="Neerajs604086@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements("requirements.txt"),
+)
