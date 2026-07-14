5e396623af2d34699ed8de2528329bcd1befe92e
Neeraj Singnath <Neerajs604086@gmail.com>
2026-07-14 23:46:06 +0530

    corrections in logging and exception and file names

--- src/logger.py
+++ src/logger.py
@@ -2,13 +2,14 @@ import logging
 import os
 from datetime import datetime
 
-LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
-logs_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+LOG_FILE = f"{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.log"
+
+logs_path = os.path.join(PROJECT_ROOT, "logs")
 os.makedirs(logs_path, exist_ok=True)
 
 LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
-
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
