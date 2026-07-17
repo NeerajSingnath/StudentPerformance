7ee5aea65b7857aa881668be30c74b9450f86e0f
Neeraj Singnath <Neerajs604086@gmail.com>
2026-07-17 20:15:56 +0530

    deployement configuration

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def predict_datapoint():
         custom_data = CustomData(
             gender=request.form.get('gender')   ,
             race_ethnicity=request.form.get('ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
+            parental_level_of_education=request .form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
             reading_score=int(request.form.get('reading_score')),
